a6b87fba1cce2f66b98ad482f5d102dbd210143c
Henry Doan <henrydoan96@gmail.com>
2019-02-25 20:58:11 -0700

    end of lecture

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,4 +12,41 @@ class PagesController < ApplicationController
     # Page.create in database
     @page = Page.new # in memory
   end
+
+  def create
+    @page = Page.new(pages_params)
+
+    if @page.save
+      redirect_to pages_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @page = Page.find(params[:id])
+  end
+
+  def update
+    @page = Page.find(params[:id])
+
+    if @page.update(pages_params)
+      redirect_to pages_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Page.find(params[:id]).destroy
+    redirect_to pages_path
+  end
+
+  private 
+    def pages_params
+      params.require(:page).permit(:title, :author, :body)
+    end
+    # { page: { title: 'foo', author: 'body', body: 'this' }}
+    # { title: 'foo', author: 'body', body: 'this' }
+    # Page.new({ title: 'foo', author: 'body', body: 'this' })
 end
